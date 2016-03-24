9c14b52cc2ba8cee69e532c6a8243ea56ff7c699
idinkova <inna.dinkova@gmail.com>
2016-03-24 21:51:00 +0200

    Added files via upload

---
+++ FootballLeague/FootballLeague/Models/League.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class League
+{
+    private static List<Team> teams = new List<Team>();
+    private static List<Match> matches = new List<Match>();
+    public static List<Team> Teams
+    {
+        get { return teams; }
+    }
+
+    public static List<Match> Matches
+    {
+        get { return matches; }
+    }
+
+
+    public static void AddMatch(Match match)
+    {
+        if (MatchAddCheck(match))
+        {
+            throw new InvalidOperationException("Match already exists in the league!");
+        }
+        Matches.Add(match);
+    }
+
+    private static bool MatchAddCheck(Match match)
+    {
+        return Matches.Any(m => m.Id == match.Id);
+    }
+
+    public static void AddTeam(Team team)
+    {
+        if (TeamAddCheck(team))
+        {
+            throw new InvalidOperationException("Team already exists in the league!");
+        }
+
+        Teams.Add(team);
+    }
+
+    private static bool TeamAddCheck(Team team)
+    {
+        return Teams.Any(t => t.Name == team.Name);
+    }
+}
---
+++ FootballLeague/FootballLeague/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballLeague
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            while (input != "End")
+            {
+                try
+                {
+                    LeagueManager.HandleInput(input);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
